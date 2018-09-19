68eba393cb192e4485c4eacc682a868c1bd7dd0b
Annette Yeager <annetteyeager@Annettes-MacBook-Pro.local>
2018-09-19 13:57:45 -0500

    stuff

--- models/Article.js
+++ models/Article.js
@@ -1,35 +1,29 @@
 // Require mongoose
-var mongoose = require("mongoose");
-var Note = require("./Note");
-// Create Schema class
-var Schema = mongoose.Schema;
-
-// Create article schema
-var ArticleSchema = new Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  summary: {
-    type: String,
-    required: true
-  },
-  link: {
-    type: String,
-    required: true
-  },
-  saved: {
-    type: Boolean,
-    default: false
-  },
-  notes: [{
-     type: Schema.Types.ObjectId,
-     ref: "Note"
-  }]
+var mongoose=require("mongoose");
+var Schema=mongoose.Schema;
+var ArticleSchema=new Schema({
+    title:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    link:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    summary:{
+        type:String
+    },
+    comments:[{
+        type:Schema.Types.ObjectId,
+        ref:"Comment"
+    }],
+    saved:{
+        type:Boolean,
+        required:true,
+        default:false
+    }
 });
-
-// Create the Article model with the ArticleSchema
-var Article = mongoose.model("Article", ArticleSchema);
-
-// Export the model
-module.exports = Article;
+var Article=mongoose.model("Article",ArticleSchema);
+module.exports=Article;
